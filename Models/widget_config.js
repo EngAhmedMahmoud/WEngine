d4403bdfcdd81ff1ddb16d2586a6f76fd14619be
EngAhmedMahmoud <ahmed.m.web.dev@gmail.com>
2019-02-28 16:09:07 +0400

    WidgetSchema

---
+++ Models/widget_config.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+const WidgetConfig = new schema({
+    name: {
+        type: String,
+        required: [true, "Widget Name Required!"],
+        index: true,
+        unique: true
+    },
+    variableName: {
+        type: String,
+        required: [true, "VariableName Required!"]
+    },
+    description: {
+        type: String,
+        default: "description"
+    },
+    version: {
+        type: String,
+        required: [true, "Version Required!"]
+    },
+    minFoundationVersion: {
+        type: String,
+        required: [true, "Minimum Foundation Version Required!"]
+    },
+    entry_point: {
+        type: String,
+        required: [true, "Entry Point Required!"]
+    },
+    dep_drivers: [
+        {
+            variable_name: String,
+            version: String
+        }
+    ],
+    dep_widgets: [
+        {
+            variable_name: String,
+            version: String
+        }
+    ],
+    size: {
+        min: {
+            width: Number,
+            height: Number
+        },
+        max: {
+            width: Number,
+            height: Number
+        }
+    },
+    location: {
+        x: String,
+        y: String
+    },
+    langs: {
+        en_US: Boolean,
+        ar_EG: Boolean,
+        es_ES: Boolean,
+        de_DE: Boolean,
+        fr_FR: Boolean,
+    },
+    styles: [String],
+    scripts: [String],
+    visibility:{
+        type:Boolean,
+        default:1
+    }
+
+});
+module.exports = mongoose.model('Widget', WidgetConfig, "widgets");
