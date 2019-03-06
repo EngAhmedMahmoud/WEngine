2eaee3cc6635f42e2035b2d66a69f5ac2ba18c00
EngAhmedMahmoud <ahmed.m.web.dev@gmail.com>
2019-03-06 17:25:44 +0400

    Setting view-engine

--- server.js
+++ server.js
@@ -5,27 +5,18 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const widgetRouter = require("./router")
 const DB_CONNECTION = require("./utils/DatabaseConnection");
-const Widget = require("./WidgetInterface");
 const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST;
 
 //init connection
 DB_CONNECTION.connection;
-//widget 
-//Widget.getWidget("http://download2262.mediafire.com/stxoem78h5tg/k6453kpdzsi942a/camera_view.zip","camera_view");
-//console.log(Widget.getWidgetConfiguration("camera_view"));
-//console.log(Widget.checkDriverDependancy("camera_view"));
-//console.log(Widget.checkWidgetDependancy("camera_view"));
-//console.log(Widget.checkEntryPoint("camera_view"));
-//console.log(Widget.saveWidget("camera_view"));
-//console.log(Widget.listDependantDrivers(["driver1","driver2"],"camera_view"));
-//console.log(Widget.listDependantDrivers(["camera_driver","dell_driver"],"camera_view"));
-//console.log(Widget.validateWidgetHierarcy("camera_view"));
 
 //creating express application
 const app = express();
-app.use(express.static(path.join(__dirname,"public")));
+app.set('view engine', 'pug');
+app.set("views", [path.join(__dirname, "views")]);
 
+app.use(express.static(path.join(__dirname,"public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/",widgetRouter);
