a976ed7bfc4fe114be4a7de38b6a6174209ca5f1
EngAhmedMahmoud <ahmed.m.web.dev@gmail.com>
2019-03-11 15:09:27 +0400

    Testing new functions

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const widgetRouter = require("./router")
 const DB_CONNECTION = require("./utils/DatabaseConnection");
 const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST;
+const WidgetEngine = require("./WidgetInterface");
 
 //init connection
 DB_CONNECTION.connection;
@@ -16,6 +17,9 @@ const app = express();
 app.set('view engine', 'pug');
 app.set("views", [path.join(__dirname, "views")]);
 
+//WidgetEngine
+// console.log(WidgetEngine.versionComapre('1.1.1','2.0.0'));
+//WidgetEngine.widgetFoundationVersion('widgey_data');
 //static files for custom pages by widgetName
 //
 app.use(express.static(path.join(__dirname,"public")));
