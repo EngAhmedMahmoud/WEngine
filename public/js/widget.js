b72ae85cf4a18c45a7b7cf2550f597f94f036f8c
EngAhmedMahmoud <ahmed.m.web.dev@gmail.com>
2019-03-20 11:03:24 +0400

    Adding styles,scripts

---
+++ public/js/widget.js
@@ -0,0 +1,30 @@
+"use strict"
+const hide = ['fa-eye-slash', 'text-muted'];
+const show = ['fa-eye', 'text-success'];
+const URL  = 'http://127.0.0.1:3000/widgetVisiability';
+
+
+function showHide(w_id) {
+    // switch activation icon
+    const widget = $(`#${w_id}`);
+    var data = { "widgetName" : `${w_id}`};
+        $.ajax({
+            type: "POST",
+            url: URL,
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function(data){
+                let state = data.data;
+                if (state) {
+                    widget.removeClass(hide).addClass(show);
+                } else {
+                    widget.removeClass(show).addClass(hide);
+                }
+            },
+            failure: function(errMsg) {
+                alert(errMsg);
+            }
+        });
+    
+}
