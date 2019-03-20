bf96f05390cfb94759acd0841b61e112b96f4eff
EngAhmedMahmoud <ahmed.m.web.dev@gmail.com>
2019-03-20 11:01:17 +0400

    update installed drivers

--- getData.js
+++ getData.js
@@ -1,11 +1,21 @@
 "use strict";
 let getData ={
     installedDrivers : ()=>{
-        let installedDrivers = [];
+        let installedDrivers =  [
+            "abst_driver",
+            "barco_cms_driver",
+            "camera_driver",
+            "dell_driver",
+            "host_manager",
+            "message_broadcast",
+            "shiv_driver",
+            "test_driver",
+            "testvar"
+        ];
         return installedDrivers;
     },
     installedWidgets : ()=>{
-        let installedWidgets = ["btWidget","camera_view","layout_widget"];
+        let installedWidgets = ["btWidget","layout_widget"];
         return installedWidgets;
     },
     getFoundationVersion:()=>{
